0d85064e5d538ebca7588d3a01ea38daf0785c9f
Weiyuan Pan <ssjack2001@msn.com>
2021-05-27 08:14:32 +0900

    update update.sh

--- ubuntu/update.sh
+++ ubuntu/update.sh
@@ -14,10 +14,7 @@ print_title Update Homebrew and packages ...
 brew upgrade -v
 print_message "Done\n"
 
-print_title Update zplug packages ...
-export ZPLUG_HOME=/home/linuxbrew/.linuxbrew/opt/zplug
-. $ZPLUG_HOME/init.zsh
-LANG=en_US.UTF-8 zplug update
+print_title "Use 'LANG=en_US.UTF-8 zplug update' to update zplug package(s)"
 print_message "Done\n"
 
 print_title "Use prefix + U to updates tpm plugin(s)"
